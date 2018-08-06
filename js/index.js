ca8de2ee8d9fd009d7d119493f487babf32e8c11
Mr.Maple <daijiale@users.noreply.github.com>
2018-08-06 12:45:10 +0800

    update

--- js/index.js
+++ js/index.js
@@ -30,24 +30,10 @@ var menu = new Vue({
 });
 
 
-var home = new Vue({
-    el: '#home',
+var slogan = new Vue({
+    el: '#slogan',
     data: {
-        home: '首页',
-        service:'服务',
-        product:'产品',
-        recruitment:'招聘',
-        partner:'伙伴',
-        lab:'Github|晨雾Lab',
-        community:'Slack|晨雾社区',
-        language:'English',
-        homeurl: 'index.html',
-        serviceurl:'服务',
-        producturl:'产品',
-        recruitmenturl:'招聘',
-        partnerurl:'伙伴',
-        laburl:'https://github.com/mornmist',
-        communityurl:'https://mornmist.slack.com',
-        languageurl:'English',
+        slogantitle: '让更多人看见并迈入雾计算时代',
+        slogansubtitle:'专注于雾存储、边缘计算、分布式计算、网格计算等科技产品落地',
     }
 });
